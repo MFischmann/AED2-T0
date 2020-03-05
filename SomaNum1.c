a38e4810ecacb2d97e6f3131317bd79846a43ad0
MFischmann <micael.fischmann@gmail.com>
2020-03-05 10:11:13 -0300

    :sparkles: Adiciona vasculhamento

--- SomaNum1.c
+++ SomaNum1.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 void main(int argc, char const *argv[])
-{
-    int c = contaSomatorios(15);
-    printf("\n15: %d \n", c);
+{   
+    int max_i = 0;
+    int max_count = 0;
+    int c;
+    for (int i = 0; i <= 30000; i++){
+        c = contaSomatorios(i);
+        if (c > max_count){
+            max_i = i;
+            max_count = c;
+            printf("%d : %d\n", max_i, max_count);
+        }
+        else if (i%1000 == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+    printf("Maximo count encontrado: %d em %d.\n", max_count, max_i);
 }
 
 int contaSomatorios(int n){
